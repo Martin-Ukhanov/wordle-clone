2a1fb714f0c29fca35a5fffffff277e1ef194f6f
Martin <martin.ukhanov@gmail.com>
2022-08-10 19:00:06 -0400

    Created tile factory

---
+++ src/modules/tile.js
@@ -0,0 +1,23 @@
+const createTile = () => {
+  let value;
+  let status;
+
+  const getValue = () => value;
+  const setValue = (newValue) => {
+    value = newValue;
+  };
+
+  const getStatus = () => status;
+  const setStatus = (newStatus) => {
+    status = newStatus;
+  };
+
+  return {
+    getValue,
+    setValue,
+    getStatus,
+    setStatus,
+  };
+};
+
+export default createTile;
